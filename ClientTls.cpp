a37f0170ce3d8826e2089a3a2af19f7034fd0b90
EricsSourceCode <162734842+EricsSourceCode@users.noreply.github.com>
2024-03-08 10:31:43 -0700

    Add files via upload

---
+++ ClientTls.cpp
@@ -0,0 +1,73 @@
+// Copyright Eric Chauvin 2022 - 2024.
+
+
+
+// This is licensed under the GNU General
+// Public License (GPL).  It is the
+// same license that Linux has.
+// https://www.gnu.org/licenses/gpl-3.0.html
+
+
+#include "ClientTls.h"
+#include "TlsOuterRec.h"
+#include "Handshake.h"
+#include "../CppBase/Casting.h"
+#include "../CppBase/CharBuf.h"
+
+
+
+
+bool ClientTls::startTestVecHandshake(
+                        const CharBuf& urlDomain,
+                        const CharBuf& port )
+{
+return tlsMainCl.startTestVecHandshake(
+                             urlDomain, port );
+}
+
+
+
+bool ClientTls::startHandshake(
+                        const CharBuf& urlDomain,
+                        const CharBuf& port )
+{
+return tlsMainCl.startHandshake(
+                          urlDomain, port );
+}
+
+
+
+Int32 ClientTls::processData( void )
+{
+try
+{
+Int32 status = tlsMainCl.processIncoming();
+
+if( status < 0 )
+  return -1;
+
+Int32 status2 = tlsMainCl.processOutgoing();
+
+if( status == 0 )
+  {
+  // Let it time out and close.
+  return 0;
+  }
+
+return status2;
+
+}
+catch( const char* in )
+  {
+  StIO::putS(
+         "Exception in ClientTls.processData:" );
+  StIO::putS( in );
+  return -1;
+  }
+catch( ... )
+  {
+  StIO::putS(
+          "Exception in ClientTls.processData" );
+  return -1;
+  }
+}
---
+++ DerEncodeLoop.cpp
@@ -0,0 +1,59 @@
+// Copyright Eric Chauvin 2023 - 2024.
+
+
+
+// This is licensed under the GNU General
+// Public License (GPL).  It is the
+// same license that Linux has.
+// https://www.gnu.org/licenses/gpl-3.0.html
+
+
+
+#include "DerEncodeLoop.h"
+#include "../CppBase/StIO.h"
+
+
+
+Int32 DerEncodeLoop::readAllTags(
+                   const CharBuf& cBuf,
+                   const Int32 where,
+                   CharBuf& statusBuf,
+                   const Int32 level )
+{
+bool constructed = false;
+DerEncode derEncode;
+
+u16Buf.appendU16( DerEncode::LevelDelim );
+u16Buf.appendU16( level & 0xFFFF );
+
+Int32 next = where;
+const Int32 reasonableCount = 100;
+for( Int32 loops = 0; loops < reasonableCount;
+                                   loops++ )
+  {
+  next = derEncode.readOneTag( cBuf, next,
+                            constructed,
+                            statusBuf,
+                            // u16Buf,
+                            level );
+  if( next < 0 )
+    return next;
+
+  if( constructed )
+    {
+    CharBuf innerBuf;
+    derEncode.getValue( innerBuf );
+
+    // Recursive for all inner tags.
+    // Int32 result =
+    readAllTags( innerBuf, 0, statusBuf,
+                               level + 1 );
+    // if( result < 0 )
+
+    // statusBuf.appendCharPt( "End of level.\n" );
+
+    }
+  }
+
+return -1;
+}
